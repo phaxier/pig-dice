e94ca3f808cce2bf61fac7ad35db5828bd4ffdd4
Noel <noelkbor@gmail.com>
2019-04-06 11:42:31 +0300

    working formula but still needs editing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 $().ready(()=>{
+var rollResult = [];
+var rollResult2 = [];
+var rollResult3 = [];
+var rollResult4 = [];
+var result1 = [0];
+
+$("#roll").click(()=>{
+event.preventDefault();
+
+var result1 = Math.floor((Math.random()*6) +1);
+Result1.unshift(randomNumber);
+alert();
+});
+if (randomNumber>1){
+    randomNumber + 1;
+   rollResult2.push(rollResult)
+   alert("pok")
+};
+
+});
