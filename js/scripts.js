a1a46e51c168c3f20f62a062ac99d9a312f43d31
Noel <noelkbor@gmail.com>
2019-04-08 08:27:55 +0300

    checking on the variable if it is working on the formula

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $("#roll").click(()=>{
 event.preventDefault();
 
 var result1 = Math.floor((Math.random()*6) +1);
-Result1.unshift(randomNumber);
+        name: '',
         role:[]
 });
 if (randomNumber>1){
