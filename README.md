1eb9e7e136a17355b35846d8b54bc1b8cb0319db
Noel <noelkbor@gmail.com>
2019-04-08 12:13:23 +0300

    images added and others changed

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-pig dice
+PIG DICE
+
+
+Brief Description
+This is a simple game that entails two players and rolling the die.
+
+
+Description
+
+Pig Dice is a simple dice game.Below are the game rules:
+
+Each player repeatedly rolls a die in each turn until either a 1 is rolled or the player decides to "hold".
+
+If the player rolls a 1,they score nothing and it becomes the next players turn.If the player rolls any other number.it is added to their turn total and the player's turn continues.In case a player chooses to "hold",their turn total is added to their score,it then becomes the next player's turn.The first player to score 100 or more points wins.
+
+Setup/Installation Requirements
+The game is played on any web browser. It also can support any mobile device.
+
+
+Known Bugs
+
+No known bugs.
+
+BDD.
+
+html javascript bootstrap jQuery
+
+Behaviour	Input	Outcome
+The two players insert	Player1:""	Goes to game
+their names	Player2:""	console
+click START
+:-----------------------------	:---------------------	:-------------------------
+Player1 clicks roll button	click roll the dice	the dice is rolled
+and a number is
+generated.
+:----------------------------	:---------------------	:-------------------------
+If player1 rolls a number	Roll=5	Round score=5
+other than 1,its added to total		
+score.		
+:----------------------------	:--------------------	:-------------------------
+If player1 rolls a 1,             the game resets and player's turn ends.
+player2 begins
+:-----------------------------	:--------------------	:--------------------
+
+:-----------------------------	:--------------------	:--------------------------
+The first player to get to 100		Winner page!!
+or more is the winner	Player1 total score=100
+:-----------------------------	:---------------------	:-------------------------
+
+Support and contact details
+In case of any issues or concerns,contact me through my cellphone at 0726571112 or through email at noelkbor@gmail.com
+
+License
+MIT
+
+Copyright (c) {year} *Noek Bor*
--- js/scripts.js
+++ js/scripts.js
@@ -1,113 +1,114 @@
 
+$().ready(()=>{
 
-    var objectPlayer = {
-        name: '',
-        role:[]
+      var objectPlayer = {
+          name: '',
+          role:[]
 
-    }
-    var objectPlayer2 = {
-        name: '',
-        role:[]
+      }
+      var objectPlayer2 = {
+          name: '',
+          role:[]
 
-    }
-     function getSum(a,b){
-        return a + b;
-    }
+      }
+       function getSum(a,b){
+          return a + b;
+      }
 
 
-    //Player 1
-    //add name
-document.getElementById('addname').addEventListener('click', function(){
-  objectPlayer.name = document.getElementById('name').value;
-  document.getElementById('inner').innerHTML = objectPlayer.name
+      //Player 1
+      //add name
+  document.getElementById('addname').addEventListener('click', function(){
+    objectPlayer.name = document.getElementById('name').value;
+    document.getElementById('inner').innerHTML = objectPlayer.name
+  });
+  // Role Dice
+  document.getElementById('roll').addEventListener('click', function(){
+      var roleArray = objectPlayer.role
+      var random = Math.ceil((Math.random()*6)+0)
+      var randomNumber = roleArray.unshift(random);
+      var total = objectPlayer.role.reduce(getSum)
+      var recent = objectPlayer.role[0];
+  document.getElementById('recent').innerHTML= recent
+     if(roleArray[0] > 1){
+
+         document.getElementById('inner1').innerHTML = roleArray
+  document.getElementById('total').innerHTML = total
+
+
+
+     }
+
+     else{
+      delete roleArray
+      delete objectPlayer.name
+      //document.getElementById('inner').remove('active')
+      //document.getElementById('inner1').remove('active')
+      var result = "Game Over!"
+      document.getElementById('result').innerHTML = result
+      document.getElementById('roll').disabled = true
+      document.getElementById('addname').disabled = true
+      document.getElementById('addname1').disabled = false
+      document.getElementById('roll1').disabled = false
+     }
+
+
+
+  });
+     //Player 2
+     //add name
+     document.getElementById('addname1').addEventListener('click', function(){
+    objectPlayer2.name = document.getElementById('name1').value;
+    document.getElementById('inner2').innerHTML = objectPlayer2.name
+  });
+  // Role Dice
+  document.getElementById('roll1').addEventListener('click', function(){
+      var roleArray1 = objectPlayer2.role
+      var random = Math.ceil((Math.random()*6)+0)
+      var randomNumber = roleArray1.unshift(random);
+      var total1 = objectPlayer2.role.reduce(getSum)
+      var recent = objectPlayer2.role[0];
+  document.getElementById('recent2').innerHTML= recent
+     if(roleArray1[0] > 1){
+
+         document.getElementById('inner3').innerHTML = roleArray1
+  document.getElementById('total1').innerHTML = total1
+  console.log(objectPlayer2.role.reduce(getSum))
+
+
+     }
+
+     else{
+      delete roleArray2
+      delete objectPlayer2.name
+      //document.getElementById('inner2').remove('active')
+      //document.getElementById('inner3').remove('active')
+
+      var result = "Game Over!"
+      document.getElementById('result1').innerHTML = result
+      document.getElementById('roll').disabled = true
+      document.getElementById('addname').disabled = true
+      document.getElementById('addname1').disabled = true
+      document.getElementById('roll1').disabled = true
+      winner()
+     }
+
+
+
+  });
+
+  //who is the winner
+  function winner(){
+  var win1 = objectPlayer.role.reduce(getSum)
+  var win2 = objectPlayer2.role.reduce(getSum)
+  if(win1>win2)
+  {
+      var textWinner =  "Player1 has won"
+      document.getElementById('winner').innerHTML = textWinner
+  }
+  else{
+      var textWinner2 =   "Player2 has won"
+      document.getElementById('winner').innerHTML = textWinner2
+  }
+  }
 });
-// Role Dice
-document.getElementById('roll').addEventListener('click', function(){
-    var roleArray = objectPlayer.role
-    var random = Math.ceil((Math.random()*6)+0)
-    var randomNumber = roleArray.unshift(random);
-    var total = objectPlayer.role.reduce(getSum)
-    var recent = objectPlayer.role[0];
-document.getElementById('recent').innerHTML= recent
-   if(roleArray[0] > 1){
-
-       document.getElementById('inner1').innerHTML = roleArray
-document.getElementById('total').innerHTML = total
-
-
-
-   }
-
-   else{
-    delete roleArray
-    delete objectPlayer.name
-    //document.getElementById('inner').remove('active')
-    //document.getElementById('inner1').remove('active')
-    var result = "Game Over!"
-    document.getElementById('result').innerHTML = result
-    document.getElementById('roll').disabled = true
-    document.getElementById('addname').disabled = true
-    document.getElementById('addname1').disabled = false
-    document.getElementById('roll1').disabled = false
-   }
-
-
-
-});
-   //Player 2
-   //add name
-   document.getElementById('addname1').addEventListener('click', function(){
-  objectPlayer2.name = document.getElementById('name1').value;
-  document.getElementById('inner2').innerHTML = objectPlayer2.name
-});
-// Role Dice
-document.getElementById('roll1').addEventListener('click', function(){
-    var roleArray1 = objectPlayer2.role
-    var random = Math.ceil((Math.random()*6)+0)
-    var randomNumber = roleArray1.unshift(random);
-    var total1 = objectPlayer2.role.reduce(getSum)
-    var recent = objectPlayer2.role[0];
-document.getElementById('recent2').innerHTML= recent
-   if(roleArray1[0] > 1){
-
-       document.getElementById('inner3').innerHTML = roleArray1
-document.getElementById('total1').innerHTML = total1
-console.log(objectPlayer2.role.reduce(getSum))
-
-
-   }
-
-   else{
-    delete roleArray2
-    delete objectPlayer2.name
-    //document.getElementById('inner2').remove('active')
-    //document.getElementById('inner3').remove('active')
-
-    var result = "Game Over!"
-    document.getElementById('result1').innerHTML = result
-    document.getElementById('roll').disabled = true
-    document.getElementById('addname').disabled = true
-    document.getElementById('addname1').disabled = true
-    document.getElementById('roll1').disabled = true
-    winner()
-   }
-
-
-
-});
-
-//who is the winner
-function winner(){
-var win1 = objectPlayer.role.reduce(getSum)
-var win2 = objectPlayer2.role.reduce(getSum)
-if(win1>win2)
-{
-    var textWinner =  "Player1 has won"
-    document.getElementById('winner').innerHTML = textWinner
-}
-else{
-    var textWinner2 =   "Player2 has won"
-    document.getElementById('winner').innerHTML = textWinner2
-}
-}
-}
